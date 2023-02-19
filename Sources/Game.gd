65cf92236eff071bbc107acac32f9221c55bf644
cydeamon <cydeamon@gmail.com>
2023-02-19 18:58:53 +0300

    Dash

--- Sources/Game.gd
+++ Sources/Game.gd
@@ -5,3 +5,13 @@ func _ready():
 
 func _process(delta):
 	$Camera.flip_offset = $Player/RobotTop.rotation.y < 0
+	draw_dash_ui()
+	
+func draw_dash_ui():
+	if $Player.is_dashing:
+		$UI/UI/HBoxContainer/DashLabel.text = "Dashing"	
+	else:
+		if $Player.is_dash_cooling_down:
+			$UI/UI/HBoxContainer/DashLabel.text = "Dash cooling down: "	+ str(round($Player/Timers/DashCooldown.time_left))
+		else:
+			$UI/UI/HBoxContainer/DashLabel.text = "Dash is ready"
--- Sources/GameObjects/Player/Player.gd
+++ Sources/GameObjects/Player/Player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody3D
 
+##########################################################################
+##########################################################################
+##########################################################################
+
 @export var speed = 5.0
 @export var jump_velocity = 4.5
 
@@ -12,10 +16,23 @@ extends CharacterBody3D
 @export var ability_active_lazer = false
 @export var ability_active_flame = false
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@export_subgroup("Dash")
+@export var dash_speed = 10
+
+##########################################################################
+##########################################################################
+##########################################################################
+
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var double_jump_used = false
 var target_rotation_y = 0
+
+var is_dash_cooling_down = false
+var is_dashing = false
+
+##########################################################################
+##########################################################################
+##########################################################################
 
 func _ready():
 	$RobotTop/AnimationPlayer.play("Idle")
@@ -24,6 +41,15 @@ func _process(delta):
 	if hook_object:
 		hook_object.global_transform = $RobotTop/Armature/MiddleBody/HookPoint.global_transform
 		
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	else:
+		double_jump_used = false
+		
+	# Handle direction rotation	
 	if velocity.x != 0:
 		if velocity.x < 0:
 			target_rotation_y = -PI / 2
@@ -33,12 +59,11 @@ func _process(delta):
 	if target_rotation_y:
 		$RobotTop.rotation.y = move_toward($RobotTop.rotation.y, target_rotation_y, delta * 10)
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	else:
-		double_jump_used = false
+	# Handle Dash.
+	if Input.is_action_just_pressed("dash") && !is_dashing && !is_dash_cooling_down:
+		is_dashing = true
+		$Timers/DashTimer.start()
+		$RobotTop/AnimationPlayer.play("DashStart")
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump"):
@@ -51,6 +76,7 @@ func _physics_process(delta):
 		if can_jump:
 			velocity.y = jump_velocity
 			$AnimationPlayer.play("JumpLight")
+			is_dashing = false
 		
 		
 
@@ -58,6 +84,7 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y))
+	
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
@@ -65,5 +92,24 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 
+	if is_dashing:
+		if target_rotation_y > 0:
+			velocity = Vector3(dash_speed, 0, 0)
+		else:
+			velocity = Vector3(-dash_speed, 0, 0)
+		
 	move_and_slide()
-	$RobotBottom/AnimationPlayer.speed_scale = velocity.x / speed
+	$RobotBottom/AnimationPlayer.speed_scale = direction.x
+
+
+func _on_dash_cooldown_timeout():
+	is_dash_cooling_down = false
+
+
+func _on_dash_timer_timeout():
+	is_dash_cooling_down = true
+	is_dashing = false
+	$Timers/DashCooldown.start()
+	$RobotTop/AnimationPlayer.stop()
+	$RobotTop/AnimationPlayer.play("DashEnd")
+	$RobotTop/AnimationPlayer.queue("Idle")
